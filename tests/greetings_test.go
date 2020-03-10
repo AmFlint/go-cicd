6b243472e7ce432aaf4068b314a50a77c71d0b2a
Antoine Masselot <amasselot@online.net>
2020-03-10 11:21:55 +0100

    debug ci

--- tests/greetings_test.go
+++ tests/greetings_test.go
@@ -21,21 +21,25 @@ func TestGreetingsHandler(t *testing.T) {
 		response, err := http.Get(fmt.Sprintf("%s?person=%s", endpoint, person))
 		if err != nil {
 			t.Errorf("Got error: %v", err)
+			return
 		}
 
 		bodyBytes, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			t.Errorf("got error while reading response body: %v", err)
+			return
 		}
 
 		greetings := greetingsResponse{}
 		if err = json.Unmarshal(bodyBytes, &greetings); err != nil {
 			t.Errorf("got error while unmarshalling response body: %v", err)
+			return
 		}
 
 		expected := fmt.Sprintf("Hello, %s", person)
 		if greetings.Message != expected {
 			t.Errorf("got message %s, expected %s", greetings.Message, expected)
+			return
 		}
 	})
 
@@ -43,21 +47,25 @@ func TestGreetingsHandler(t *testing.T) {
 		response, err := http.Get(endpoint)
 		if err != nil {
 			t.Errorf("Got error: %v", err)
+			return
 		}
 
 		bodyBytes, err := ioutil.ReadAll(response.Body)
 		if err != nil {
 			t.Errorf("got error while reading response body: %v", err)
+			return
 		}
 
 		greetings := greetingsResponse{}
 		if err = json.Unmarshal(bodyBytes, &greetings); err != nil {
 			t.Errorf("got error while unmarshalling response body: %v", err)
+			return
 		}
 
 		expected := "Hello, World"
 		if greetings.Message != expected {
 			t.Errorf("got message %s, expected %s", greetings.Message, expected)
+			return
 		}
 	})
 }
